7e9658a5b71b51fdab1836aec42d47fb9be6e7c8
ensiyehpaseban <ensiyehpaseban@gmail.com>
2024-10-03 12:20:38 +0330

    task2.py

---
+++ task2.py
@@ -0,0 +1,40 @@
+data={
+    ("Singupore",1)
+    ("Irland",6)
+    ("United Kingdome",7)
+    ("Germany",27)
+    ("Armenia",34)
+    ("United State",17)
+    ("Canada",9)
+    ("Italy",74)
+}
+
+country=str(input("enter your country:"))
+
+if  country=="Singupore":
+    print(data.get("Singupore"))
+
+elif country=="Irland":
+    print(data.get("Irland"))
+
+elif country=="United Kingdome":
+    print(data.get("United Kingdome"))
+
+elif country=="Germany":
+    print(data.get("Germany"))
+
+elif country=="Armenia":
+    print(data.get("Armenia"))
+
+elif country=="United State":
+    print(data.get("United State"))
+
+elif country=="Canada":
+    print(data.get("Canada"))
+
+elif country=="Italy":
+    print(data.get("Italy"))
+
+
+
+    
